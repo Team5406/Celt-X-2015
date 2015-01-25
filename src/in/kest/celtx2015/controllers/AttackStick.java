a395a59848c9405ee21b0d746feb80fa9c4cb020
Kestin Goforth <kgoforth1503@gmail.com>
2015-01-25 15:22:55 -0500

    Added main stick functions

--- src/in/kest/celtx2015/controllers/AttackStick.java
+++ src/in/kest/celtx2015/controllers/AttackStick.java
@@ -1,9 +1,19 @@
 package in.kest.celtx2015.controllers;
 
 public class AttackStick extends Controller {
+	
+	public static final int TRIGGER = 1;
 
 	public AttackStick(int port) {
 		super(port);
+	}
+	
+	public double getMainX(){
+		return super.getRawAxis(0);
+	}
+	
+	public double getMainY(){
+		return super.getRawAxis(1);
 	}
 
 }
