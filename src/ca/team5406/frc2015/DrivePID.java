f748a1ba792547878daa7b1099e4e1a2eabfe2e4
Kestin Goforth <kgoforth1503@gmail.com>
2015-01-28 20:41:37 -0500

    Created sperate drivePID class

---
+++ src/ca/team5406/frc2015/DrivePID.java
@@ -0,0 +1,27 @@
+package ca.team5406.frc2015;
+
+public class DrivePID {
+	
+	//Declare DriveTo, TurnTo PIDs
+
+	public DrivePID(){
+		//Init PIDs
+	}
+	
+	public void resetDriveToPos(){
+		
+	}
+	
+	public boolean driveToPos(int pos){
+		return false;
+	}
+	
+	public void resetTurnToAngle(){
+		
+	}
+	
+	public boolean turnToAngle(double angle){
+		return false;
+	}
+	
+}
